faabcf4934c9bd1fc3599516c219d35061b74e81
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-02-22 12:18:32 +0600

    first problem solved

---
+++ practice-prob-1.js
@@ -0,0 +1,13 @@
+// number 1
+const multi3 = (a = 1, b = 1, c = 1) => a * b * c;
+console.log(multi3(2, 5));
+
+// number 2
+const threeLines = `I am a web developer.
+I love to code.
+I love to eat biriyani.`
+console.log(threeLines)
+
+// number 3
+const defaultPara = (a=0, b=0) => a+b;
+console.log(defaultPara(5));
